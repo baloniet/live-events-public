95a2d3c758ce983364f8265261d5e97ad1d4ef43
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2016-12-16 09:16:09 +0100

    rc1

--- src/app/shared/base.url.ts
+++ src/app/shared/base.url.ts
@@ -1,2 +1,3 @@
-export const BASE_API_URL = 'http://localhost:3005';
+//export const BASE_API_URL = 'http://localhost:3005';
+export const BASE_API_URL = 'http://193.2.173.83:20030';
 export const API_VERSION = 'api';
