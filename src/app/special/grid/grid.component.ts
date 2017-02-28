daa9dc27bad16ac06b0f0f4218534e5dde31db56
Lavric Janez <janez.lavric@rtvslo.si>
2017-02-28 17:04:09 +0100

    RC6

--- src/app/special/grid/grid.component.ts
+++ src/app/special/grid/grid.component.ts
@@ -1,3 +1,6 @@
+import { VMeventApi } from './../../shared/sdk/services/custom/VMevent';
+import { VAmember } from './../../shared/sdk/models/VAmember';
+import { VMeinApi } from './../../shared/sdk/services/custom/VMein';
 import { VAmemberApi } from './../../shared/sdk/services/custom/VAmember';
 import { ActivatedRoute } from '@angular/router';
 import { LabelService } from './../../services/label.service';
@@ -14,12 +17,15 @@ export class GridComponent extends BaseFormComponent implements OnInit {
 
   private actId;
   private events;
+  private eids;
+  private members;
 
   constructor(
     private _labelService: LabelService,
     private _route: ActivatedRoute,
     private _eventApi: EventApi,
-    private _memberApi: VAmemberApi
+    private _memberApi: VAmemberApi,
+    private _eApi: VMeventApi
   ) {
     super('event', 'grid')
   }
@@ -32,13 +38,37 @@ export class GridComponent extends BaseFormComponent implements OnInit {
   selectData(param) {
     if (param) {
       this.actId = param.id;
-      
+
       this._eventApi.find({ order: ["starttime"], where: { "activityId": this.actId } })
-        .subscribe(res => {this.events = res; this.prepareMembers()}, this.errMethod);
+        .subscribe(res => {
+          this.events = res;
+          this.eids = [];
+          for (let r of res)
+            this.eids.push(r['id']);
+          this.prepareMembers()
+        }, this.errMethod);
     }
   }
 
-  prepareMembers(){
-    this._memberApi.find({})
+  prepareMembers() {
+    this._memberApi.find({ order: ["lastname", "firstname"], where: { "id": this.actId } })
+      .subscribe(res => {
+        this.members = [];
+        for (let m of res) {
+          this._eApi.find({ order: ["starttime"], where: { "activityId": this.actId, "personId": (<VAmember>m).personId } })
+            .subscribe(res => {
+
+              let evts = this.eids.slice();
+
+              for (let e of res) {
+                let index = evts.indexOf(e['id']);
+                if (index > -1)
+                  evts[index]={ ack: e['padate'], off: e['podate'], test: 1 };
+              }
+              this.members.push({ lastname: (<VAmember>m).lastname, events: evts })
+
+            }, this.errMethod);
+        }
+      }, this.errMethod);
   }
 }
